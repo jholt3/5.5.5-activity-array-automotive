d0c7f7fe332f66bacd326392a7048e4e13b4cbcd
Jay Holt <jholt3@Jays-MacBook-Pro.local>
2022-05-19 07:34:16 -0400

    added functionality to mercurySedan.js file

--- mecurySedan.js
+++ mecurySedan.js
@@ -6,7 +6,7 @@ class Car extends VehicleModule {
     constructor(make, model, year, color, mileage) {
         super(make, model, year, color, mileage);
         this.maximumPassengers = 5;
-        this.passenger = 2;
+        this.passenger = 3;
         this.numberOfWheels = 4;
         this.maximumSpeed = 160;
         this.fuel = 10;
